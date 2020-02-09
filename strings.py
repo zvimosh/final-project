fc43493acebfd8a0a12ed74a2a2ac9a232d76c3e
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2020-02-09 17:40:35 +0200

    added git commit for jenkins
    added git commit for jenkins

--- strings.py
+++ strings.py
@@ -7,7 +7,7 @@ Create a function that returns the words in reverse order:
 Example: ['circus', 'flying', 'pythons', 'monty']
 Create a function that returns a list of 4 character strings:
 Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
-
+### git comment
 """
 import pytest
 
