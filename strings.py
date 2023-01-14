054b7796736efe569a155bf2eae74c5d59f7e663
zvimosh <zvimoshko@gmail.com>
2023-01-14 18:27:22 +0200

    fixed strings code and added code proof of work

--- strings.py
+++ strings.py
@@ -11,16 +11,27 @@ Example: ['mont', 'y py', 'thon', 's fl', 'ying', ' cir', 'cus']
 """
 import pytest
 
+a_string = 'monty pythons flying circus'
 def no_duplicates(a_string):
+    '''Returns a sorted string with no duplicate characters'''
+    none_duplicates = ''.join(sorted(set(a_string.lower())))
+    return none_duplicates
     pass
 
 
 def reversed_words(a_string):
-    pass
+    '''Returns the words in reverse order'''
+    a_list = a_string.split()
+    reversed_list = a_list[::-1]
+    return reversed_list
+    
 
 
 def four_char_strings(a_string):
-    pass
+    '''Returns a list of 4 character strings'''
+    four_char = ([a_string[i:i+4] for i in range(0, len(a_string), 4)])
+    return four_char
+
 
 
 def test_no_duplicates():
@@ -44,4 +55,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+""" print('--------------------------------------')
+    print('task 1 - none duplicates, sorted')
+    print('--------------------------------------')
+    print(no_duplicates(a_string))
+    print('--------------------------------------')
+    print('task 2 - reversed words')
+    print('--------------------------------------')
+    print(reversed_words(a_string))
+    print('--------------------------------------')
+    print('task 3 - 4 char strings')
+    print('--------------------------------------')
+    print(four_char_strings(a_string))
+    print('--------------------------------------')
+    retcode = pytest.main([__file__])
+     """
+    
     
