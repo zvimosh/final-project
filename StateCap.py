3d9136908627dfb40acb36f223d8937b1f523650
zvimosh <zvimoshko@gmail.com>
2023-01-14 15:16:00 +0200

    fixed code "StateCap" + upload proof

--- StateCap.py
+++ StateCap.py
@@ -12,7 +12,6 @@ handle that?
 
 """
 import sys
-
 import pytest
 
 STATES_CAPITALS = {
@@ -65,31 +64,88 @@ STATES_CAPITALS = {
     'Washington' : 'Olympia',
     'West Virginia' : 'Charleston',
     'Wisconsin' : 'Madison',
-    'Wyoming' : 'Cheyenne',
+    'Wyoming' : 'Cheyenne'
 }
 
 
 def capital_of_Idaho():
-    # Your code here
+    '''Print the state capital of Idaho'''
+    if len(STATES_CAPITALS) == 0:
+        raise KeyError
+    print(STATES_CAPITALS['Idaho'])
     pass
 
 def all_states():
-    # Your code here
+    '''Print all states'''
+    if len(STATES_CAPITALS) == 0:
+        raise KeyError
+    for state in STATES_CAPITALS:
+        print(state)
     pass
 
 def all_capitals():
-    # Your code here
+    '''Print all capitals'''
+    if len(STATES_CAPITALS) == 0:
+        raise KeyError
+    for capital in STATES_CAPITALS.values():
+        print(capital)
     pass
 
 def states_capitals_string():
-    # Your code here
-    pass
+    '''Print a single string of the list 'Alabama -> Montgomery, Alaska -> Juneau, ...' '''
+    if len(STATES_CAPITALS) == 0:
+        raise KeyError
+    all_strings = ""
+    sorted_dict = dict(sorted(STATES_CAPITALS.items()))
+    for state, capital in sorted_dict.items():
+        all_strings +=state + " -> " + capital + ", "
+        #all_strings.sort() 
+    print(all_strings[0:-2])
+    return(all_strings[0:-2])
 
 
 
 def get_state(capital):
-    pass
+    '''Given a capital, return the state, if there are multiple states with the same capital, return a list of states'''
+    #matched_state = list(sorted_dict.keys())[list(sorted_dict.values()).index(capital)]
+    #print(matched_state)
+    if len(STATES_CAPITALS) == 0:
+        return KeyError
+    sorted_dict = dict(sorted(STATES_CAPITALS.items()))
+    matched_state = []
+    for country, city in sorted_dict.items():
+        if capital == city:
+            matched_state.append(country)
+    if  len(matched_state) == 1:
+        print (matched_state[0])
+        return(matched_state[0])
+    elif len(matched_state) > 1:
+        print(matched_state)
+        return(matched_state)
+    else:
+        print("No match found")
+        raise KeyError
+        
 
+
+def get_capital(state):
+    '''Given a state, return the capital, if there are multiple capitals with the same state, return a list of captials'''
+    #matched_capitol = list(sorted_dict.values())[list(sorted_dict.keys()).index(state)]
+    #print(matched_capitol)
+    if len(STATES_CAPITALS) == 0:
+        return KeyError
+    sorted_dict = dict(sorted(STATES_CAPITALS.items()))
+    matched_capital = []
+    for country, city in sorted_dict.items():
+        if state == country:
+            matched_capital.append(city)
+    if  len(matched_capital) == 1:
+        print (matched_capital[0])
+    elif len(matched_capital) > 1:
+        print(matched_capital)
+    else:
+        print("No match found")
+        raise KeyError
 
 
 def test_state_to_capital():
@@ -115,4 +171,30 @@ def main():
 
 
 if __name__ == '__main__':
+    '''Manual Outputs for testing and proof of work'''
+"""     print('--------------------------------------')
+    print('task 1 - captial of Idaho')
+    print('--------------------------------------')
+    capital_of_Idaho()
+    print('--------------------------------------')
+    print('task 2 - all states')
+    print('--------------------------------------')
+    all_states()
+    print('--------------------------------------')
+    print('task 3 - all capitals')
+    print('--------------------------------------')
+    all_capitals()
+    print('--------------------------------------')
+    print('task 4 - all states and capitals')
+    print('--------------------------------------')
+    states_capitals_string()
+    print('--------------------------------------')
+    print('task 5 - get state from capital')
+    print('--------------------------------------')
+    get_state('Cheyenne')
+    print('--------------------------------------')
+    print('task 6 - get capital from state')
+    print('--------------------------------------')
+    get_capital('Wyoming')
+    retcode = pytest.main([__file__]) """
     sys.exit(main())
